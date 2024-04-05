a56aea0ae8c32fefc72a2d719488a3ea1f701836
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-04-05 23:09:26 +0800

    initial commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/LoadingIndicator.css";
+
+function LoadingIndicator() {
+  return (
+    <div className="loading-container">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
